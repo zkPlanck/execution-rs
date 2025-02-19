9f9003c8a6781beb0c2bbd54e8d76fdc9dd71b75
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2025-02-19 13:28:02 +0545

    Initial commit

---
+++ src/state/account_state.rs
@@ -0,0 +1,8 @@
+use crate::types::ctype::H160;
+
+// Account state
+struct AccountState{
+ balance:u64,
+ address:H160
+}
+
---
+++ src/state/transaction_state.rs
@@ -0,0 +1,9 @@
+use crate::types::ctype::H160;
+
+// Transaction state
+struct Transaction{
+    nonce:u64,
+    to:H160,
+    value:u64
+}
+
